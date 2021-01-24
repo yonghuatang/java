33acef24033440dabcb61655509e79ae4aab1eb4
Yong Hua Tang <tang.yonghua010111@gmail.com>
2021-01-24 14:34:38 +0800

    update

---
+++ MyClass2.java
@@ -0,0 +1,10 @@
+public class MyClass2 {
+    static int MyMethod(int i, int j) {  // a function, similar to C++
+        return i + j;
+    }
+    public static void main(String[] args) {
+        System.out.println(MyMethod(2, 3));
+    }
+}
+
+//Extra: 'final' keyword is similar to 'const' in C++, denote a fixed variable.
